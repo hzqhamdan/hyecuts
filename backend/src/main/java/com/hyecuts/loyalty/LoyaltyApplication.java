7cbe9dab1a0a179b9279ecdae0cc2b30303943ac
Haziq Hamdan <haziqhanafi7@gmail.com>
2026-05-28 16:55:10 +0800

    debug: force binding to 0.0.0.0 and add startup port logging to resolve 502 error

--- backend/src/main/java/com/hyecuts/loyalty/LoyaltyApplication.java
+++ backend/src/main/java/com/hyecuts/loyalty/LoyaltyApplication.java
@@ -5,7 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class LoyaltyApplication {
+
     public static void main(String[] args) {
-        SpringApplication.run(LoyaltyApplication.class, args);
+        org.springframework.context.ConfigurableApplicationContext context = SpringApplication.run(LoyaltyApplication.class, args);
+        String port = context.getEnvironment().getProperty("server.port");
+        String address = context.getEnvironment().getProperty("server.address");
+        System.out.println(">> HYECUTS Backend is running on " + address + ":" + port);
     }
+
 }
